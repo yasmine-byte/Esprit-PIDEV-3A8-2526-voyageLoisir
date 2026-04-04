eb0fb6cb02b6eeacce72f3b12b608a3df2ce2ecb
Farah <Farah.Shimi@esprit.tn>
2026-04-04 21:04:18 +0100

    Module Activite complet (front + back + UI)

---
+++ src/Controller/AdminActiviteController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Activite;
+use App\Form\ActiviteType;
+use App\Repository\ActiviteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/activite')]
+class AdminActiviteController extends AbstractController
+{
+    #[Route('', name: 'admin_activite_index', methods: ['GET'])]
+    public function index(ActiviteRepository $activiteRepository): Response
+    {
+        return $this->render('admin/activite/index.html.twig', [
+            'activites' => $activiteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'admin_activite_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $activite = new Activite();
+        $form = $this->createForm(ActiviteType::class, $activite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($activite);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_activite_index');
+        }
+
+        return $this->render('admin/activite/new.html.twig', [
+            'activite' => $activite,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_activite_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Activite $activite, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ActiviteType::class, $activite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_activite_index');
+        }
+
+        return $this->render('admin/activite/edit.html.twig', [
+            'activite' => $activite,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_activite_delete', methods: ['POST'])]
+    public function delete(Request $request, Activite $activite, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$activite->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($activite);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_activite_index');
+    }
+}
